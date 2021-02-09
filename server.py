2af64195fe751d43c21747c90213a008a3ab2ca0
Daniel Kluver <kluve018@umn.edu>
2021-02-09 16:48:40 -0600

    added a back-end for that lovely new front-end.

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, g
+from flask import Flask, render_template, request, g, redirect, url_for
 
 import db
 
@@ -14,10 +14,19 @@ def home():
     user_name = request.args.get("userName", "unknown")
     return render_template('main.html', user=user_name)
 
-@app.route('/people')
+@app.route('/people', methods=['GET'])
 def people():
     with db.get_db_cursor() as cur:
         cur.execute("SELECT * FROM person;")
         names = [record[1] for record in cur]
 
         return render_template("people.html", names=names)
+
+@app.route('/people', methods=['POST'])
+def new_person():
+    with db.get_db_cursor(True) as cur:
+        name = request.form.get("name", "unnamed friend")
+        app.logger.info("Adding person %s", name)
+        cur.execute("INSERT INTO person (name) values (%s)", (name,))
+        
+        return redirect(url_for('people'))
