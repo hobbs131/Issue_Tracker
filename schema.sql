c0d2f0ab59f9ac7767bac70976653618963de206
Daniel Kluver <kluve018@umn.edu>
2021-02-09 14:44:30 -0600

    changed schema to something a little more interesting

--- schema.sql
+++ schema.sql
@@ -1,16 +1,22 @@
 drop table person;
-drop table skill;
+drop table gift_idea;
 
-create table person(
+create table person (
   person_id SERIAL PRIMARY KEY,
-  name varchar(255)
+  name varchar(255) NOT NULL
 );
 
-create table skill(
-  skill_id SERIAL PRIMARY KEY,
+create table gift_idea (
+  gift_idea_id SERIAL PRIMARY KEY,
   person_id int references person,
-  skill varchar(255)
+  product varchar(255) NOT NULL,
+  external_link varchar(255)
 );
 
 insert into person (name) values ('Yang He');
 insert into person (name) values ('Daniel Kluver');
+
+-- I know a weird number of people named laura
+insert into person (name) values ('Laura');
+insert into person (name) values ('Laura (house)');
+insert into person (name) values ('Laura (family)');
