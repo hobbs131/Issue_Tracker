1cfe44c8de572e988d6e809962efe8a96ae79067
Nixon4Ever <samdiode2@gmail.com>
2021-03-03 15:01:04 -0600

    server edit

--- server.py
+++ server.py
@@ -12,45 +12,45 @@ app.secret_key = os.environ["FLASK_SECRET_KEY"]
 # have the DB submodule set itself up before we get started. groovy.
 @app.before_first_request
 def initialize():
-    db.setup()
-    auth0_setup()
+	db.setup()
+	auth0_setup()
 
 
 ### AUTH:
 @app.route('/login')
 def login():
-    if 'profile' in session:
-        return redirect('/issues')
-    else:
-        return auth0().authorize_redirect(redirect_uri=url_for('callback', _external=True))
+	if 'profile' in session:
+		return redirect('/issues')
+	else:
+		return auth0().authorize_redirect(redirect_uri=url_for('callback', _external=True))
 
 
 @app.route('/logout')
 def logout():
-    session.clear()
-    params = { 'returnTo': url_for('home', _external=True), 'client_id': os.environ['AUTH0_CLIENT_ID'] }
-    return redirect(auth0().api_base_url + '/v2/logout?' + urlencode(params))
+	session.clear()
+	params = { 'returnTo': url_for('home', _external=True), 'client_id': os.environ['AUTH0_CLIENT_ID'] }
+	return redirect(auth0().api_base_url + '/v2/logout?' + urlencode(params))
 
 
 @app.route('/callback')
 def callback():
-    auth0().authorize_access_token()
-    resp = auth0().get('userinfo')
-    userinfo = resp.json()
+	auth0().authorize_access_token()
+	resp = auth0().get('userinfo')
+	userinfo = resp.json()
 
-    session['jwt_payload'] = userinfo
-    session['profile'] = {
-        'user_id': userinfo['sub'],
-        'name': userinfo['name'],
-        'picture': userinfo['picture']
-    }
+	session['jwt_payload'] = userinfo
+	session['profile'] = {
+		'user_id': userinfo['sub'],
+		'name': userinfo['name'],
+		'picture': userinfo['picture']
+	}
 
-    return redirect('/issues')
+	return redirect('/issues')
 
 
 @app.route('/')
 def home():
-     return redirect(url_for('login'))
+	return redirect(url_for('login'))
 
 
 @app.route('/issues')
@@ -65,7 +65,7 @@ def issues():
 
 @app.route('/add_issue')
 def add_issue():
-    return render_template('add_issue.html')
+	return render_template('add_issue.html')
 
 
 @app.route('/postIssueEntry', methods = ["POST"])
@@ -84,7 +84,7 @@ def postIssueEntry():
 
 @app.route('/edit_issue', methods = ["POST"])
 def editIssueEntry():
-    with db.get_db_cursor(True) as cur:
+	with db.get_db_cursor(True) as cur:
 		id = int(request.form.get('id'))
 		issue = request.form.get('issue')
 		priority = request.form.get('priority')
@@ -98,8 +98,8 @@ def editIssueEntry():
 
 @app.route('/delete', methods = ["POST"])
 def deleteIssueEntry():
-    with db.get_db_cursor(True) as cur:
-        data = request.json
-        id = data['issueId']
-        cur.execute("UPDATE issues  SET  deletedAt = now()  WHERE  id = %s" % id)
-        return ""
+	with db.get_db_cursor(True) as cur:
+		data = request.json
+		id = data['issueId']
+		cur.execute("UPDATE issues  SET  deletedAt = now()  WHERE  id = %s" % id)
+		return ""
